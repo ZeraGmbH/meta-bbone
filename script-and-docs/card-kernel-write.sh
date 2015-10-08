4f42211a9d7ce005c95b2f5ecc43167c12ab837a
Andreas Müller <schnitzeltony@googlemail.com>
2015-10-08 17:10:42 +0200

    add scripts to write images/kernels to sdcard
    Taken from meta-gumstix-community - I hate this dd/sdcard-image crap
    other bsps use.
    
    Signed-off-by: Andreas Müller <schnitzeltony@googlemail.com>

---
+++ script-and-docs/card-kernel-write.sh
@@ -0,0 +1,145 @@
+#! /bin/bash
+
+# card-kernel-write.sh
+# (c) Copyright 2013-2015 Andreas Müller <schnitzeltony@googlemail.com>
+# Licensed under terms of GPLv2
+#
+# This script writes kernel+modules to SDCard. To
+# select card device a dialog based GUI is used. To work properly
+# the OE environment variable OE_BUILD_TMPDIR must be set.
+
+SelectKernel() {
+	# OE environment found?
+	if [ -z $OE_BASE ]; then
+		echo "The environment variable OE_BASE is not set. It is usually set before running bitbake."
+		exit 1
+	fi
+	iCount=0
+	strSelection=
+	for grep_result in `grep -h TMPDIR $OE_BASE/conf/*.conf | sed -e s/' '/''/g -e s/'\"'/''/g`; do
+		# exclude comments
+		tmp_dir=`echo $grep_result | grep '^TMPDIR='`
+		if [ ! -z $tmp_dir ]; then
+			TMPDIR=`echo $tmp_dir | sed -e s/'TMPDIR='/''/g`
+		fi
+	done
+	for BuildPath in ${TMPDIR}-*; do
+		for i in `find ${BuildPath}/deploy/images/${MACHINE} -name ${KERNEL_IMAGE_TYPE}'-*-*-*.bin' | sort` ; do
+			iCount=`expr $iCount + 1`
+			KernelNameArr[${iCount}]=$i
+			strSelection="$strSelection $iCount "`basename $i`
+		done
+	done
+
+	# were files found?
+	if [ $iCount -eq 0 ]; then
+		echo "No kernel images found in ${OE_BUILD_TMPDIR}-\*"
+		exit 1
+	fi
+
+	dialog --title 'Select kernel'\
+	--menu 'Move using [UP] [DOWN],[Enter] to select' 30 100 $iCount\
+	${strSelection}\
+	2>/tmp/menuitem.$$
+
+	# get OK/Cancel
+	sel=$?
+	# get selected menuitem
+	menuitem=`cat /tmp/menuitem.$$`
+	rm -f /tmp/menuitem.$$
+
+	# Cancel Button or <ESC>
+	if [ $sel -eq 1 -o $sel -eq 255 ] ; then
+		echo Cancel selected 1
+		return 1
+	fi
+	KernelImage=${KernelNameArr[$menuitem]}
+}
+
+run_user() {
+	if [ -z $DevicePath ]; then
+		# DevicePath for memory card
+		SelectCardDevice || exit 1
+	fi
+
+	if [ -z $KernelImage ]; then
+		# select rootfs
+		SelectKernel || exit 1
+	fi
+	RootParams="$DevicePath $KernelImage $KERNEL_IMAGE_TYPE"
+}
+
+run_root() {
+	# device node valid?
+	if [ ! -b $DevicePath ] ; then
+		echo "$DevicePath is not a valid block device!"
+		exit 1
+	fi
+	# rootfs valid?
+	if [ ! -e $KernelImage ] ; then
+		echo "$KernelImage can not be found!"
+		exit 1
+	fi
+
+	IMAGEDIR=$(dirname $KernelImage)
+
+	# check if the card is currently mounted
+	MOUNTSTR=$(mount | grep $DevicePath)
+	if [ -n "$MOUNTSTR" ] ; then
+	    echo -e "\n$DevicePath is currenly mounted. Needs unmounting..."
+	    umount -f ${DevicePath}?*
+	fi
+
+	# create temp mount path
+	if [ ! -d /tmp/tmp_mount$$ ] ; then
+		mkdir /tmp/tmp_mount$$
+	fi
+
+	# boot partition
+	echo "Writing kernel to boot partition"
+	mount ${DevicePath}1 /tmp/tmp_mount$$ || exit 1
+	cp $KernelImage /tmp/tmp_mount$$/$KernelImageType
+	sleep 1
+	umount ${DevicePath}1 || exit 1
+
+	# rootfs
+	echo "Writing kernel to rootfs"
+	mount ${DevicePath}2 /tmp/tmp_mount$$ || exit 1
+	cd /tmp/tmp_mount$$/boot
+	rm -f [zu]Image*
+	# kernel
+	KernelFileName=`basename $KernelImage`
+	KernelDirName=`dirname  $KernelImage`
+        KernelShortFileName=${KernelFileName%*-*-*-*}
+	cp $KernelImage $KernelShortFileName
+	ln -sf $KernelShortFileName $KernelImageType
+	# modules
+	echo "Writing modules to rootfs"
+	cd ..
+	ModulesFileName=`echo $KernelFileName | sed -e s/${KernelImageType}/modules/ -e s/\.bin/\.tgz/`
+	ModulesName="${KernelDirName}/${ModulesFileName}"
+	tar xvzf $ModulesName lib/modules
+
+	cd ..
+	umount ${DevicePath}2 || exit 1
+
+	rm -rf /tmp/tmp_mount$$
+}
+
+. `dirname $0`/tools.inc
+
+if [ -z $MACHINE ]; then
+	MACHINE=$DEFAULT_MACHINE
+fi
+if [ -z $KERNEL_IMAGE_TYPE ]; then
+	KERNEL_IMAGE_TYPE=$DEFAULT_KERNEL_IMAGE_TYPE
+fi
+
+DevicePath=$1
+KernelImage=$2
+KernelImageType=$3
+
+# On the 1st call: run user
+# After the 2nd call: run root
+RootParams='$DevicePath $KernelImage $KERNEL_IMAGE_TYPE'
+chk_root "The kernel images on %DevicePath% will be overwritten!!" && run_root
---
+++ script-and-docs/card-part.sh
@@ -0,0 +1,100 @@
+#! /bin/bash
+
+# part-card.sh
+# (c) Copyright 2013 Andreas Müller <schnitzeltony@googlemail.com>
+# Licensed under terms of GPLv2
+#
+# This script prepares partitions on SDCards. It wraps
+# http://omappedia.org/wiki/Minimal-FS_SD_Configuration by dialog based GUI.
+
+# for debugging set DEBUG=echo
+DEBUG=
+
+# SelectRootFileSystem() creates a dialog to select one of all available removable
+# devices. The path to the selected device is stored in the variale DevicePath.
+SelectRootFileSystem() {
+	strSelection="0 ext4 1 ext3"
+	dialog --title 'Select rootfs type'\
+	--menu 'Move using [UP] [DOWN],[Enter] to select' 10 100 2\
+	${strSelection}\
+	2>/tmp/menuitem.$$
+
+	# get OK/Cancel
+	sel=$?
+	# get selected menuitem
+	menuitem=`cat /tmp/menuitem.$$`
+	rm -f /tmp/menuitem.$$
+
+	# Cancel Button or <ESC>
+	if [ $sel -eq 1 -o $sel -eq 255 ] ; then
+		echo 'Cancel selected SelectRootFileSystem()'
+		return 1
+	fi
+	# Selection
+	case $menuitem in
+		0) RootfsType='ext4';;
+		1) RootfsType='ext3';;
+		*) echo "Unknown type"; exit 1;;
+	esac
+}
+
+run_user() {
+	if [ -z $DevicePath ]; then
+		SelectCardDevice || exit 1
+	fi
+	if [ -z $RootfsType ]; then
+		SelectRootFileSystem || exit 1
+	fi
+	if [ $# -gt 2 ]; then
+		echo "Usage: $0 [Card device path] [Rootfs type]"
+		exit 1;
+	fi
+	RootParams="$DevicePath $RootfsType"
+}
+
+run_root() {
+	# device node valid?
+	if [ ! -b $DevicePath ] ; then
+		echo "$DevicePath is not a valid block device!"
+		exit 1
+	fi
+
+	# file system valid?
+	case $RootfsType in
+		ext3) ;;
+		ext4) ;;
+		*) echo "Unknown rootfs type"; exit 1;;
+	esac
+
+	# check if the card is currently mounted
+	MOUNTSTR=$(mount | grep $DevicePath)
+	if [ -n "$MOUNTSTR" ] ; then
+	    echo -e "\n$DevicePath is currenly mounted. Needs unmounting..."
+	    $DEBUG umount -f ${DevicePath}?*
+	fi
+
+	$DEBUG dd if=/dev/zero of=$DevicePath bs=1024 count=1024
+
+	# force 512 bytes / sector
+	export LC_ALL=C
+	SIZE=`fdisk -l $DevicePath | grep Disk | grep bytes | awk '{print $5}'`
+	echo "Disk size: $SIZE bytes"
+	CYLINDERS=`echo $SIZE/255/63/512 | bc`
+	echo "Cylinders: $CYLINDERS"
+	# setup partitions
+	{
+	echo ,9,0x0C,*
+	echo ,,,-
+	} | $DEBUG sfdisk -D -H 255 -S 63 -C $CYLINDERS $DevicePath
+	# write partitions
+	$DEBUG mkfs.vfat -F 32 -n "boot" -s 2 ${DevicePath}1
+	$DEBUG mke2fs -j -t $RootfsType -L "rootfs" ${DevicePath}2
+}
+
+
+. `dirname $0`/tools.inc
+DevicePath=$1
+RootfsType=$2
+# On the 1st call: run user
+# After the 2nd call: run root
+chk_root && run_root
